c65db43eee3ddd430333ce2f40a40361d05ffc86
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-09-14 17:00:18 -0600

    Modify postcreatecommand

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
-mkdir -p /usr/local/bin \
-    && curl -SL https://github.com/dbt-labs/dbt-cli/releases/download/v0.29.5/dbt_0.29.5_linux_arm64.tar.gz \
-    | tar -xJC /usr/local/bin \
-    && make -C /usr/local/bin all
-
-PATH="${PATH}:/usr/local/bin/dbt"
+curl -LO https://github.com/dbt-labs/dbt-cli/releases/download/v0.29.5/dbt_0.29.5_linux_arm64.tar.gz
+tar -xzf dbt_0.29.5_linux_arm64.tar.gz
+CURRENT_DIR=$(pwd)
+export PATH=$PATH:$CURRENT_DIR
+echo "export PATH=\$PATH:$CURRENT_DIR" >> ~/.bashrc
+source ~/.bashrc
