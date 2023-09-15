d32fff6d5cf4664dab35b1fe825103b56b380a65
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-09-15 14:10:49 -0600

    Update bash commands

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
-curl -LO https://github.com/dbt-labs/dbt-cli/releases/download/v0.29.5/dbt_0.29.5_linux_arm64.tar.gz
-tar -xzf dbt_0.29.5_linux_arm64.tar.gz
-CURRENT_DIR=$(pwd)
-export PATH=$PATH:$CURRENT_DIR
-echo "export PATH=\$PATH:$CURRENT_DIR" >> ~/.bashrc
+BIN_DIR="/usr/local/bin"
+curl -LO https://github.com/dbt-labs/dbt-cli/releases/download/v0.29.5/dbt_0.29.5_linux_amd64.tar.gz
+sudo tar -xzf dbt_0.29.5_linux_amd64.tar.gz -C $BIN_DIR dbt
+export PATH=$PATH:$$BIN_DIR
+echo "export PATH=\$PATH:$BIN_DIR" >> ~/.bashrc
 source ~/.bashrc
