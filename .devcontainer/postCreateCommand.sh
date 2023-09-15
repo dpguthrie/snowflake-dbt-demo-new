8b9a16dc50e51d818c3dbb1c91cf7811d0768fe8
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-09-15 14:24:10 -0600

    Update commands and add secrets

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,4 +3,5 @@ curl -LO https://github.com/dbt-labs/dbt-cli/releases/download/v0.29.5/dbt_0.29.
 sudo tar -xzf dbt_0.29.5_linux_amd64.tar.gz -C $BIN_DIR dbt
 export PATH=$PATH:$$BIN_DIR
 echo "export PATH=\$PATH:$BIN_DIR" >> ~/.bashrc
+echo "export DBT_CLOUD_API_KEY=$DBT_CLOUD_API_KEY" >> ~/.bashrc
 source ~/.bashrc
