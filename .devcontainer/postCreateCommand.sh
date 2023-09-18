2fcefc01230cf718e81e69ff63b663ddd49646e3
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-09-18 13:26:42 -0600

    Add metricflow to the mix

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,7 +3,8 @@ curl -LO https://github.com/dbt-labs/dbt-cli/releases/download/v0.29.5/dbt_0.29.
 sudo tar -xzf dbt_0.29.5_linux_amd64.tar.gz -C $BIN_DIR dbt
 export PATH=$PATH:$$BIN_DIR
 echo "export PATH=\$PATH:$BIN_DIR" >> ~/.bashrc
-echo "export DBT_CLOUD_API_KEY=$DBT_CLOUD_API_KEY" >> ~/.bashrc
 rm dbt_0.29.5_linux_amd64.tar.gz
 . ~/.bashrc
 pip install -r .devcontainer/requirements.txt
+mkdir ~/.dbt/
+./.devcontainer/create_profile.py > ~/.dbt/profiles.yml
