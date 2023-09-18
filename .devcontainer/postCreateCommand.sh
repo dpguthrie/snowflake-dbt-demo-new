86832ba6481823037a368cf4b8cdc21f7578004d
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-09-18 13:37:17 -0600

    Remove metricflow stuff for now

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,5 +6,3 @@ echo "export PATH=\$PATH:$BIN_DIR" >> ~/.bashrc
 rm dbt_0.29.5_linux_amd64.tar.gz
 . ~/.bashrc
 pip install -r .devcontainer/requirements.txt
-mkdir ~/.dbt/
-./.devcontainer/create_profile.py > ~/.dbt/profiles.yml
