9d68e1d0cf0983b109a6314d6fc736b87e4d8a33
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-10-05 10:01:30 -0600

    Increase CLI version

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,6 +3,6 @@ curl -LO https://github.com/dbt-labs/dbt-cli/releases/download/v0.29.5/dbt_0.29.
 sudo tar -xzf dbt_0.29.5_linux_amd64.tar.gz -C $BIN_DIR dbt
 export PATH=$PATH:$$BIN_DIR
 echo "export PATH=\$PATH:$BIN_DIR" >> ~/.bashrc
-rm dbt_0.29.6_linux_amd64.tar.gz
+rm dbt_0.30.0_linux_amd64.tar.gz
 . ~/.bashrc
 pip install -r .devcontainer/requirements.txt
