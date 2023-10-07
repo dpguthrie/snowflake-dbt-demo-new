56e413d8f7dc90d3a48b05b80bdf7e90ec351f41
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-10-07 11:28:58 -0600

    Bump dbt-cloud-cli version

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 BIN_DIR="/usr/local/bin"
 curl -LO https://github.com/dbt-labs/dbt-cli/releases/download/v0.29.5/dbt_0.29.5_linux_amd64.tar.gz
-sudo tar -xzf dbt_0.29.5_linux_amd64.tar.gz -C $BIN_DIR dbt
+sudo tar -xzf dbt_0.30.2_linux_amd64.tar.gz -C $BIN_DIR dbt
 export PATH=$PATH:$$BIN_DIR
 echo "export PATH=\$PATH:$BIN_DIR" >> ~/.bashrc
 rm dbt_0.30.0_linux_amd64.tar.gz
