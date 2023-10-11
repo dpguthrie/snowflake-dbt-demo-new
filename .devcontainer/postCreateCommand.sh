659c355043b151596bec96c95f726e4dc88269f4
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-10-11 12:17:18 -0600

    Bump version to 0.32.2

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 BIN_DIR="/usr/local/bin"
-CLI_VERSION="0.31.2"
+CLI_VERSION="0.32.2"
 curl -LO "https://github.com/dbt-labs/dbt-cli/releases/download/v${CLI_VERSION}/dbt_${CLI_VERSION}_linux_amd64.tar.gz"
 sudo tar -xzf "dbt_${CLI_VERSION}_linux_amd64.tar.gz" -C "$BIN_DIR" dbt
 export PATH=$PATH:$$BIN_DIR
