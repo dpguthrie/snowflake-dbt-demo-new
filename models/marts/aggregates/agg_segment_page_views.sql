7a41f41524c2e9e86390ac40f1451a5a60ddde3a
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-12-04 22:04:56 -0700

    bad code

--- models/marts/aggregates/agg_segment_page_views.sql
+++ models/marts/aggregates/agg_segment_page_views.sql
@@ -6,7 +6,7 @@ select
     date_trunc('day', timestamp) as date_day,
     src as page_source,
     title as page_title,
-    count(*) as total_page_views
+    count(*) as total_page_views,
 from page_views
 where page_title not like 'DaaC'
 group by 1, 2, 3
