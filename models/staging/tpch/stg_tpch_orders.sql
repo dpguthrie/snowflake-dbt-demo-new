775e0006c79b633633a863ea2808d60b41188c63
Doug Guthrie <doug.guthrie@dbtlabs.com>
2024-01-08 16:42:31 +0000

    fix stg_tpch_orders

--- models/staging/tpch/stg_tpch_orders.sql
+++ models/staging/tpch/stg_tpch_orders.sql
@@ -13,10 +13,7 @@ renamed as (
         o_orderstatus as status_code,
         o_totalprice as total_price,
         o_orderdate as order_date,
-<<<<<<< HEAD
-=======
         o_clerk as clerk_name,
->>>>>>> dd342a6cb04e01255e16c6cdb1ed698a607abf06
         o_orderpriority as priority_code,
         o_shippriority as ship_priority,
         o_comment as comment
