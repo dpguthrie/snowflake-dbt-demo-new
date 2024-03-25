af43dd2abe90f4cb4f885df42e7e66c5a3fba5a0
doug.guthrie@dbtlabs.com <doug.guthrie@dbtlabs.com>
2024-03-25 16:56:52 +0000

    dfg

--- tests/stg_tpch_orders_assert_positive_price.sql
+++ tests/stg_tpch_orders_assert_positive_price.sql
@@ -2,7 +2,8 @@
     config(
         enabled=true,
         severity='error',
-        tags = ['finance']
+        tags = ['finance'],
+        error_if = '>200000'
     )
 }}
 
@@ -10,4 +11,4 @@ with orders as ( select * from {{ ref('stg_tpch_orders') }} )
 
 select *
 from   orders 
-where  total_price < 0
+where  total_price > 0
