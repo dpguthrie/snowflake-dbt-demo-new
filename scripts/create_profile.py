e4b2e44455fcab95b1d76cb5dff503d10fb9cc37
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-03-26 05:15:42 -0600

    Try using dbt Cloud CLI with dbt-checkpoint

--- scripts/create_profile.py
+++ scripts/create_profile.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python
 import os
+
 import yaml
 
-
-if __name__ == '__main__':
-    
-    warehouse_config = {
-        'type': 'snowflake',
-        'account': os.getenv('SF_ACCOUNT', None),
-        'user': os.getenv('SF_USER', None),
-        'password': os.getenv('SF_PASSWORD', None),
-        'role': os.getenv('SF_ROLE', 'TRANSFORMER'),
-        'database': os.getenv('SF_DATABASE', 'DOUG_DEMO_V2'),
-        'schema': os.getenv('SF_SCHEMA', 'dbt_dguthrie'),
-        'warehouse': os.getenv('SF_WAREHOUSE', 'TRANSFORMING'),
-    }
-    
-    target_name = 'prod'
-    
-    profile_config = {
-        'tpch': {
-            'outputs': {
-                target_name: warehouse_config,
-            },
-            'target': target_name,
+if __name__ == "__main__":
+    dbt_cloud_config = {
+        "version": 1,
+        "context": {
+            "active-project": os.environ["DBT_CLOUD_PROJECT_ID"],
+            "active-host": os.environ["DBT_CLOUD_HOST"],
         },
+        "projects": [
+            {
+                "project-name": os.environ["DBT_CLOUD_PROJECT_NAME"],
+                "project-id": os.environ["DBT_CLOUD_PROJECT_ID"],
+                "account-name": os.environ["DBT_CLOUD_ACCOUNT_NAME"],
+                "account-id": os.environ["DBT_CLOUD_ACCOUNT_ID"],
+                "account-host": os.environ["DBT_CLOUD_ACCOUNT_HOST"],
+                "api-key": os.environ["DBT_CLOUD_API_KEY"],
+            }
+        ],
     }
 
-    print(yaml.dump(profile_config))
+    print(yaml.dump(dbt_cloud_config))
