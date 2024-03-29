5ff0699b1d93114e4191d1460a24f2c86ee7d8a1
Doug Guthrie <doug.guthrie@dbtlabs.com>
2024-03-29 17:27:49 +0000

    new target schema

--- snapshots/tpch/tpch_supplier_snapshot.sql
+++ snapshots/tpch/tpch_supplier_snapshot.sql
@@ -1,6 +1,7 @@
 {% snapshot tpch_supplier_snapshot %}
 
 {{ config(
+    target_schema=generate_schema_name("snapshots_{{ env_var('DBT_TARGET_ENV', 'dev')"),
     unique_key='s_suppkey',
     strategy='timestamp',
     updated_at='_etl_updated_timestamp',
