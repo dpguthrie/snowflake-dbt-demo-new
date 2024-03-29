3777f4bb4213a3cf5c6e166f4e881356a8ea920e
Doug Guthrie <doug.guthrie@dbtlabs.com>
2024-03-29 17:10:54 +0000

    change snapshot location

--- snapshots/tpch/tpch_customer_snapshot.sql
+++ snapshots/tpch/tpch_customer_snapshot.sql
@@ -1,8 +1,6 @@
 {% snapshot tpch_customer_snapshot %}
 
 {{ config(
-    target_database='doug_demo_v2',
-    target_schema='snapshots',
     unique_key='c_custkey',
     strategy='timestamp',
     updated_at='_etl_updated_timestamp',
