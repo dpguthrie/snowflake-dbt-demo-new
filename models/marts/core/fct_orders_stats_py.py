b24a4c12e00d31060e12b6f176f1df69210b99c1
Doug Guthrie <doug.guthrie@dbtlabs.com>
2024-04-01 18:44:47 +0000

    asdf

--- models/marts/core/fct_orders_stats_py.py
+++ models/marts/core/fct_orders_stats_py.py
@@ -2,13 +2,12 @@ def model(dbt, session):
 
     # Access to config block
     dbt.config(
-        materialized='table',
-        snowflake_warehouse='SNOWPARK_WH',
-        enabled=False,
+        materialized="table",
+        snowflake_warehouse="SNOWPARK_WH",
     )
 
     # Get upstream data
-    df = dbt.ref('fct_orders')
+    df = dbt.ref("fct_orders")
 
     # Describe the data
     df = df.describe()
