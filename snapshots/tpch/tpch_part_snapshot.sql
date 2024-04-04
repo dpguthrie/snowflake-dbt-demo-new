cffa9edc8170286d623a4d39db4b40cece2299d5
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-04-04 13:41:37 -0600

    Fix snapshots

--- snapshots/tpch/tpch_part_snapshot.sql
+++ snapshots/tpch/tpch_part_snapshot.sql
@@ -1,7 +1,7 @@
 {% snapshot tpch_part_snapshot %}
 
 {{ config(
-    target_schema=generate_schema_name("snapshots_" ~ env_var('DBT_TARGET_ENV', 'dev')),
+    target_schema='snapshots',
     unique_key='p_partkey',
     strategy='timestamp',
     updated_at='_etl_updated_timestamp',
