fe0601323fac295a7de7941da22fbcdc959f4cd9
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-07-04 07:28:05 -0600

    Remove unused import

--- models/ml/forecast_train_py.py
+++ models/ml/forecast_train_py.py
@@ -1,37 +1,34 @@
-import pandas as pd
-
 from datetime import datetime
 
-from prophet import Prophet
+import pandas as pd
 from prophet.serialize import model_to_json
 
 
 def model(dbt, session):
-
     # dbt configuration
     dbt.config(
-        materialized='incremental',
-        packages=['pandas', 'prophet'],
-        snowflake_warehouse='SNOWPARK_WH',
+        materialized="incremental",
+        packages=["pandas", "prophet"],
+        snowflake_warehouse="SNOWPARK_WH",
     )
 
     # get upstream data
-    revenue = dbt.ref('revenue_weekly_by_ship_mode').to_pandas()
+    revenue = dbt.ref("revenue_weekly_by_ship_mode").to_pandas()
 
     # rename to match Prophet's expected column names
     renames = {
-        'date_week'.upper(): 'ds',
-        'ship_mode'.upper(): 'location',
-        'total_revenue'.upper(): 'y',
+        "date_week".upper(): "ds",
+        "ship_mode".upper(): "location",
+        "total_revenue".upper(): "y",
     }
     revenue = revenue.rename(columns=renames)
 
     # get list of unique locations dynamically
-    locations = sorted(list(revenue['location'].unique()))
+    locations = sorted(list(revenue["location"].unique()))
 
     # train the ML models per location
     models = [
-        Prophet().fit(revenue[revenue['location'] == location])
+        Prophet().fit(revenue[revenue["location"] == location])
         for location in locations
     ]
 
@@ -41,9 +38,9 @@ def model(dbt, session):
     # persist models -- serialize Prophet as JSON via provided method
     df = pd.DataFrame(
         {
-            'trained_at': [trained_at] * len(locations),
-            'location': locations,
-            'model': [model_to_json(model) for model in models],
+            "trained_at": [trained_at] * len(locations),
+            "location": locations,
+            "model": [model_to_json(model) for model in models],
         }
     )
 
