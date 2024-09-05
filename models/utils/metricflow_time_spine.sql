9452847ea272b887971c542c2d8c8b807dcbe645
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-09-04 20:06:29 -0600

    Add yml for time spine model

--- models/utils/metricflow_time_spine.sql
+++ models/utils/metricflow_time_spine.sql
@@ -7,10 +7,10 @@
 with days as (
 
     {{
-        dbt_utils.date_spine(
+        dbt.date_spine(
             'day',
             "to_date('01/01/2000','mm/dd/yyyy')",
-            "to_date('01/01/2027','mm/dd/yyyy')"
+            "to_date('01/01/2025','mm/dd/yyyy')"
         )
     }}
 
@@ -22,3 +22,5 @@ final as (
 )
 
 select * from final
+where date_day > dateadd(year, -4, current_timestamp()) 
+and date_hour < dateadd(day, 30, current_timestamp())
