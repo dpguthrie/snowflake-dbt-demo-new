6c3062736073e86788a679469b71b23c0a92e77b
Doug Guthrie <doug.guthrie@dbtlabs.com>
2024-10-05 13:13:31 +0000

    Add user_id to pipeline, semantic model

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -13,10 +13,10 @@ cleanup as (
         c_address as address,
         c_nationkey as nation_key,
         c_phone as phone_number,
-        c_acctbal*2 as account_balance,
+        c_acctbal as account_balance,
         c_mktsegment as market_segment,
         c_comment as comment,
-
+        user_id
 
     from source
 
