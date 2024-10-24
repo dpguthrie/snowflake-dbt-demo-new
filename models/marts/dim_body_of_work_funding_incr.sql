9d38fd80500d93ea195b51bff0df3ae73302b04b
Doug Guthrie <doug.guthrie@dbtlabs.com>
2024-10-24 15:58:21 +0000

    new way of doing dim_body_of_work_funding

--- models/marts/dim_body_of_work_funding_incr.sql
+++ models/marts/dim_body_of_work_funding_incr.sql
@@ -18,7 +18,9 @@ with latest_data as (
         bowf.logical_delete_ind,
         1 as current_record_ind,
         current_timestamp() as effective_start_ts,
-        '2099-12-31' as effective_end_ts
+        '2099-12-31' as effective_end_ts,
+        md5_number_lower64(body_of_work_funding_source_row_id || effective_start_ts) as body_of_work_funding_skey,
+        md5_number_lower64(body_of_work_funding_source_row_id || effective_start_ts) as body_of_work_funding_durable_skey,
     from {{ ref('stg_gates__body_work_funding') }} as bowf
     join {{ ref('stg_gates__strategy') }} as st
         on bowf.funding_strategy_id = st.funding_strategy_id
@@ -43,9 +45,17 @@ with latest_data as (
         latest_data.body_of_work_funding_id,
         latest_data.grant_purpose_desc,
         latest_data.logical_delete_ind,
+
+        -- Expire record's indicator
         0 as current_record_ind,
         latest_data.effective_start_ts,
-        current_timestamp() as effective_end_ts
+
+        -- Expire record's timestamp
+        current_timestamp() as effective_end_ts,
+        latest_data.body_of_work_funding_skey,
+
+        -- Use the current durable_skey for the changed record
+        current_data.body_of_work_funding_durable_skey,
     from latest_data
     left join current_data
         on latest_data.body_of_work_funding_source_row_id = current_data.body_of_work_funding_source_row_id
@@ -62,6 +72,7 @@ with latest_data as (
 
 , new_records as (
 
+    -- These are completely net new records that don't exist in the target
     select
         latest_data.*
     from latest_data
@@ -69,17 +80,26 @@ with latest_data as (
         on latest_data.body_of_work_funding_source_row_id = current_data.body_of_work_funding_source_row_id
     where current_data.body_of_work_funding_source_row_id is null
 
+    union all
+
+    -- These are the records that were changed that need to be inserted with the appropriate current record indicator of 1, durable_skey
+    select
+        latest_data.* exclude body_of_work_funding_durable_skey,
+
+        -- Ensure the durable_skey remains what it is currently
+        changed_records.body_of_work_funding_durable_skey
+    from latest_data
+    join changed_records
+        on latest_data.body_of_work_funding_source_row_id = changed_records.body_of_work_funding_source_row_id
+
 )
 
 select * from changed_records
 union all
 select * from new_records
-union all
-{% endif %}
-select * from latest_data
-{% if is_incremental() %}
-left join new_records on
-    latest_data.body_of_work_funding_source_row_id = new_records.body_of_work_funding_source_row_id
-where new_records.body_of_work_funding_source_row_id is null
-{% endif %}
 
+{% else %}
+
+select * from latest_data
+
+{% endif %}
