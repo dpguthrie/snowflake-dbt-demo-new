d8049678ef52f541dfa0e28c21c96ffa9d9b4c70
Doug Guthrie <doug.guthrie@dbtlabs.com>
2024-11-19 22:47:46 +0000

    hello world

--- models/marts/core/fct_order_items.sql
+++ models/marts/core/fct_order_items.sql
@@ -49,6 +49,7 @@ final as (
         order_item.item_tax_amount,
         order_item.net_item_sales_amount,
         order_item.gross_item_sales_amount as gross_item_sales_amount,
+        'hello world' as col
 
     from
         order_item
