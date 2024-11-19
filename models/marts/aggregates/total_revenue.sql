2cdfcc142cda6f01703601af443d2943d8a625cc
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-11-19 11:25:17 -0700

    Changing back

--- models/marts/aggregates/total_revenue.sql
+++ models/marts/aggregates/total_revenue.sql
@@ -1,5 +1,5 @@
 select
     date_trunc('month', order_date) as date_month,
     sum(gross_item_sales_amount) as total_revenue
-from doug_demo_v2.analytics.fct_orders
+from {{ ref('fct_orders') }}
 group by 1
