72fb4cb4efeed3feb28ad93471d1f7d9d605470b
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-12-20 16:09:23 -0700

    Change intermediate model

--- models/marts/aggregates/agg_parts_price.sql
+++ models/marts/aggregates/agg_parts_price.sql
@@ -1,7 +1,15 @@
 {{ config(materialized='table') }}
 
+
+with cte as (
+    select
+        manufacturer,
+        sum(retail_price) as total_retail_price
+    from {{ ref('dim_parts') }}
+    group by 1
+)2
+
 select
     manufacturer,
-    sum(retail_price) as total_retail_price
-from {{ ref('dim_parts') }}
-group by 1
+    total_retail_price + 1 as total_retail_price
+from cte
