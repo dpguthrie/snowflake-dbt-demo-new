a82a22075b497f8b40811751a5e398bc464dcb3e
Doug Guthrie <douglas.p.guthrie@gmail.com>
2025-01-04 17:30:07 -0700

    just where clause

--- models/staging/tpch/stg_tpch_parts.sql
+++ models/staging/tpch/stg_tpch_parts.sql
@@ -11,12 +11,12 @@ renamed as (
         p_partkey as part_key,
         p_name as name,
         p_mfgr as manufacturer,
-        p_brand || 's' as brand,
+        p_brand as brand,
         p_type as type,
         p_size as size,
         p_container as container,
         p_retailprice as retail_price,
-        p_comment || 's' as comment,
+        p_comment as comment,
         'hello world' as col
 
     from source
