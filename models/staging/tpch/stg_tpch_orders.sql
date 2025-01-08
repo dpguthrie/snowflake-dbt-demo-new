24fe4adf6a9499475424ec4bbf2f2ddae2dda3a5
Doug Guthrie <douglas.p.guthrie@gmail.com>
2025-01-07 21:17:34 -0700

    Modify two different models

--- models/staging/tpch/stg_tpch_orders.sql
+++ models/staging/tpch/stg_tpch_orders.sql
@@ -12,7 +12,7 @@ renamed as (
         o_custkey as customer_key,
         o_orderstatus as status_code,
         o_totalprice as total_price,
-        o_orderdate as order_date,
+        dateadd('day', 1, o_orderdate) as order_date,
         o_clerk as clerk_name,
         o_orderpriority as priority_code,
         o_shippriority as ship_priority,
