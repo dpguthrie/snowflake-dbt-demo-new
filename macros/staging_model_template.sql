a649ede2e0c83a046f1877f9d10abb00178d1127
Doug Guthrie <doug.guthrie@dbtlabs.com>
2025-03-19 14:34:50 +0000

    update template

--- macros/staging_model_template.sql
+++ macros/staging_model_template.sql
@@ -1,16 +1,3 @@
-{% macro _staging_column(
-    column,
-    column_mapping
-) %}
-
-{%- if column_mapping and column_mapping.get(column.column, {}).get("default", none) -%}
-    {%- set default = column_mapping[column.column]['default'] -%}
-{%- else -%}
-    {%- set default = var('defaults')[column.dtype] -%}
-{% endif -%}
-coalesce({{ column.column }}, {{ default }}) as {{ column.column }},
-{% endmacro %}
-
 {% macro staging_model_template(source_name, source_table, column_mapping = {}) %}
 
 {%- set rel = source(source_name, source_table) -%}
@@ -23,11 +10,41 @@ with source as (
 standardized as (
     select
     {%- for col in columns %}
-        {{ _staging_column(col, column_mapping) }}
+        {{ _staging_column(col, column_mapping.get(col.column, {})) }}
     {%- endfor %}
     from source
 )
 
 select * from standardized
 
+{% endmacro %}
+
+
+{% macro _staging_column(
+    column,
+    column_dict
+) %}
+
+{%- if column_dict.get("sql", none) -%}
+{{ column_dict["sql"] }}
+{%- else -%}
+{{ _use_coalesce_func(column, column_dict) }}
+{%- endif -%}
+{% endmacro %}
+
+{% macro _use_column_sql(sql) %}
+{{ sql }}
+{% endmacro %}
+
+{% macro _use_coalesce_func(column, column_dict) %}
+
+{%- if column_dict.get("default", none) -%}
+    {%- set default = column_dict['default'] -%}
+    {%- if column.dtype == "VARCHAR" %}
+        {%- set default = "'" ~ default ~ "'" %}
+    {%- endif -%}
+{%- else -%}
+    {%- set default = var("defaults")[column.dtype] -%}
+{% endif -%}
+coalesce({{ column.column }}, {{ default }}) as {{ column.column }},
 {% endmacro %}
